d6eb97ef8c0344d7051b4c98720144a826ea20a5
hzyhzzn <hzyhzzn@gmail.com>
2014-12-10 13:21:04 +0800

    login
    无区别role 无记住我时间限制 登录

--- ExamSystem/login.php
+++ ExamSystem/login.php
@@ -1,4 +1,12 @@
 <?php
 require("database.php");
-
+$username = mysqli_real_escape_string($con, $_POST['username']);
+$password = mysqli_real_escape_string($con, $_POST['password']);
+$result = mysqli_query($con,"SELECT * FROM user WHERE username='$username' AND password='$password'");
+#$rows = mysql_fetch_assoc($result);
+if($result){
+    echo '登录成功';
+}else{
+    echo '登录失败';
+}
 ?>
